6473ca1db953aabc4ab24336bf8fd9bd5a3a9b98
HadukiDesktop <haduki.kyouka@gmail.com>
2024-04-21 07:58:06 +0900

    数量もあったほうがいいか

--- recipecalc/src/main/java/recipecalc/node/Node.java
+++ recipecalc/src/main/java/recipecalc/node/Node.java
@@ -2,7 +2,9 @@ package recipecalc.node;
 
 public class Node {
     public final String id;
-    public Node(String id) {
+    public final long quantity;
+    public Node(String id, long quantity) {
         this.id = id;
+        this.quantity = quantity;
     }
 }
