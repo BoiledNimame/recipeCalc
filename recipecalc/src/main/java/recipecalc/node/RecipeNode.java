ed5bb45f514d0c053189fe88676052514f797f01
HadukiDesktop <haduki.kyouka@gmail.com>
2024-04-21 10:44:59 +0900

    パース不可能な部分を減らした

--- recipecalc/src/main/java/recipecalc/node/RecipeNode.java
+++ recipecalc/src/main/java/recipecalc/node/RecipeNode.java
@@ -3,35 +3,12 @@ package recipecalc.node;
 public class RecipeNode {
 
     public final String name;
-    public final String[] parent;
     public final Node[] ingredientNodes;
     public final Node[] resutNodes;
-    public final long requiredQuantity;
 
-    public RecipeNode(String name, String[] parent, Node[] ingredients, Node[] result, long quantity) {
+    public RecipeNode(String name, Node[] ingredients, Node[] result) {
         this.name = name;
-        this.parent = parent;
         ingredientNodes = ingredients;
         resutNodes = result;
-        requiredQuantity = quantity;
-    }
-
-    public boolean equals(RecipeNode node) {
-        final int selfLength = this.parent.length;
-        final int targetLength = node.parent.length;
-        if (selfLength==targetLength && selfLength!=0 ) {
-            int match = 0;
-            for (int i = 0; i < selfLength; i++) {
-                if (this.parent[i].equals(node.parent[i])) {
-                    match++;
-                    if (match==selfLength) {
-                        return true;
-                    }
-                } else {
-                    return false;
-                }
-            }
-        }
-        return false;
     }
 }
