f2f3e960f58b2c85efdde70e36ce77db0a6b18c1
HadukiLaptop <haduki.kyouka@gmail.com>
2024-04-22 09:01:13 +0900

    やっぱり簡単に呼び出せるMapがないとキツい(-> なんで最初からMapでやんなかったの?)

--- recipecalc/src/main/java/recipecalc/node/LinkedRecipeNode.java
+++ recipecalc/src/main/java/recipecalc/node/LinkedRecipeNode.java
@@ -1,6 +1,9 @@
 package recipecalc.node;
 
+import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class LinkedRecipeNode {
@@ -11,29 +14,33 @@ public class LinkedRecipeNode {
     public final RecipePos pos;
     public final LinkedRecipeNode parent;
     public final LinkedRecipeNode[] child;
-    public final LinkedRecipeNode[] resultNodes;
-    public final RecipeNode[] allRecipe;
+    public final Node[] resultNodes;
     public final long requiredQuantity;
-    public final Node[] consumableIngredients;
+
+    private static final Map<String, SimpleImmutableEntry<Node[], Node[]>> registedRecipes = new HashMap<>();
 
     /**
      * make LinkedRecipeNode as HEAD
      */
     public LinkedRecipeNode(RecipeNode targetRecipe, long requiredQuantity, RecipeNode[] allRecipe) {
+        recipeRegister(allRecipe);
         depth = 0;
         name = targetRecipe.name;
-        final Node targetNode = Arrays.stream(targetRecipe.resutNodes).filter(f -> f.id.equals(name)).findFirst().orElseThrow(IllegalArgumentException::new);
+        final Node targetNode = Arrays.stream(targetRecipe.resutNodes)
+                                        .filter(f -> f.id.equals(name))
+                                        .findFirst().orElseThrow(IllegalArgumentException::new);
         display = displayBuilder(name, targetRecipe.resutNodes[0], requiredQuantity);
         uuid = UUID.randomUUID();
         pos = RecipePos.HEAD;
         parent = null;
         child = targetRecipe.resutNodes; // TODO ここにコンストラクタ呼ぶ関数詰めて再帰的展開を行う
         resultNodes = null; // TODO いらないかも?
-        consumableIngredients = null; // TODO ここでは余剰を取り扱う予定
-        this.allRecipe = allRecipe;
     }
 
-    public LinkedRecipeNode(int depth, String targetName, long requiredQuantity, LinkedRecipeNode parent) {
+    /**
+     * Body and Tail's Constructor cannot call from other class 
+     */
+    private LinkedRecipeNode(int depth, String targetName, long requiredQuantity, LinkedRecipeNode parent) {
         uuid = UUID.randomUUID();
         pos = RecipePos.BODY;
     }
@@ -55,6 +62,12 @@ public class LinkedRecipeNode {
                 break;
         }
         return targetDisplay;
+    }
+
+    private static void recipeRegister(RecipeNode[] recipes) {
+        for (int i = 0; i < recipes.length; i++) {
+            registedRecipes.put(recipes[i].name, new SimpleImmutableEntry<Node[],Node[]>(recipes[i].ingredientNodes, recipes[i].resutNodes));
+        }
     }
 
     public boolean equals(LinkedRecipeNode node) {
