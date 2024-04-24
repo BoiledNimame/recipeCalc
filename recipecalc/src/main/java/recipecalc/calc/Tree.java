d30422a7a94595e0e10ee8fc3000b7321952c09f
HadukiDesktop <haduki.kyouka@gmail.com>
2024-04-25 01:03:27 +0900

    動いてるっぽいのでもう信頼して展開を考える

---
+++ recipecalc/src/main/java/recipecalc/calc/Tree.java
@@ -0,0 +1,10 @@
+package recipecalc.calc;
+
+import recipecalc.node.LinkedNode;
+
+public class Tree {
+    final String[] lines = new String[]{"│", "├", "─", "└"};
+    public static void printAsTree(LinkedNode origin) {
+        // TODO ツリー状に展開+最終リソースの要求数
+    }
+}
