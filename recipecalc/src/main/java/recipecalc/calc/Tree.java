d3c2a4be3a79cc473b20bfbd957231959c084a93
HadukiDesktop <haduki.kyouka@gmail.com>
2024-04-25 02:30:03 +0900

    もうちょい賢い書き方あると思うけどテストにゃこれで十分か

--- recipecalc/src/main/java/recipecalc/calc/Tree.java
+++ recipecalc/src/main/java/recipecalc/calc/Tree.java
@@ -1,10 +1,26 @@
 package recipecalc.calc;
 
 import recipecalc.node.LinkedNode;
+import java.util.Collections;
 
 public class Tree {
     final static String[] lines = new String[]{"│", "├", "─", "└"};
     public static void printAsTree(LinkedNode origin) {
         // TODO ツリー状に展開+最終リソースの要求数
+        branch(origin);
+    }
+
+    private static void branch(LinkedNode node) {
+        System.out.println(repeat(lines[0], node.depth).concat(node.display));
+        if (node.child.isEmpty()) { return; }
+        else {
+            for (LinkedNode cNode : node.child) {
+                branch(cNode);
+            }
+        }
+    }
+    
+    private static String repeat(String str, int n) {
+        return String.join("", Collections.nCopies(n, str));
     }
 }
