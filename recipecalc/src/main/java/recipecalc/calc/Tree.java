ed9b1b1ca08d3db0bcae1ab6858515590e6d3f3c
HadukiLaptop <haduki.kyouka@gmail.com>
2024-04-25 10:07:36 +0900

    個数確認とリファクタリングの準備

--- recipecalc/src/main/java/recipecalc/calc/Tree.java
+++ recipecalc/src/main/java/recipecalc/calc/Tree.java
@@ -26,7 +26,7 @@ public class Tree {
         String prefix = node.parent!=null
                       ? !node.parent.child.isEmpty()
                        ? !node.parent.child.get(node.parent.child.size()-1).equals(node)
-                        ? buildPrefix(node.parent).concat(lines[1]) // TODO repeatではなくparent再帰で行う
+                        ? buildPrefix(node.parent).concat(lines[1])
                         : buildPrefix(node.parent).concat(lines[3])
                        : Util.repeat(lines[0], node.depth)
                       : Util.repeat(lines[0], node.depth);
