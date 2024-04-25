f62da78a13599ad0c6a8a84169c240d11850b063
HadukiLaptop <haduki.kyouka@gmail.com>
2024-04-25 09:43:28 +0900

    メソッド分離

--- recipecalc/src/main/java/recipecalc/calc/Tree.java
+++ recipecalc/src/main/java/recipecalc/calc/Tree.java
@@ -6,30 +6,30 @@ import recipecalc.node.LinkedNode;
 public class Tree {
     final static String[] lines = new String[]{"│", "├", "─", "└"};
     public static void printAsTree(LinkedNode origin) {
-        (new Tree(origin)).branch(origin);
+        (new Tree()).branch(origin);
     }
 
-    private LinkedNode before;
-    private Tree(LinkedNode origin) {
-        before = origin;
+    private Tree() {
     }
 
     private void branch(LinkedNode node) {
-        // TODO beforeのdepthあるいはそれを利用したツリー構造化
-        String prefix = node.depth!=before.depth
-                      ? !node.parent.child.isEmpty()
-                       ? !node.parent.child.get(node.parent.child.size()-1).equals(node)
-                        ? Util.repeat(lines[0], node.depth - 1).concat(lines[1])
-                        : Util.repeat(lines[0], node.depth - 1).concat(lines[3])
-                       : Util.repeat(lines[0], node.depth)
-                      : Util.repeat(lines[0], node.depth);
-        System.out.println(prefix.concat(node.display));
-        before = node;
+        System.out.println(buildLine(node));
         if (node.child.isEmpty()) { return; }
         else {
             for (LinkedNode cNode : node.child) {
                 branch(cNode);
             }
         }
+    }
+
+    private String buildLine(LinkedNode node) {
+        String prefix = node.parent!=null
+                      ? !node.parent.child.isEmpty()
+                       ? !node.parent.child.get(node.parent.child.size()-1).equals(node)
+                        ? Util.repeat(lines[0], node.depth - 1).concat(lines[1]) // TODO repeatではなくparent再帰で行う
+                        : Util.repeat(lines[0], node.depth - 1).concat(lines[3])
+                       : Util.repeat(lines[0], node.depth)
+                      : Util.repeat(lines[0], node.depth);
+        return prefix.concat(node.display);
     }
 }
