5463c56fbb3a8eb4ae433b50e7c787ba2c19692f
HadukiLaptop <haduki.kyouka@gmail.com>
2024-04-26 15:42:51 +0900

    出力の整列

--- recipecalc/src/main/java/recipecalc/calc/Tree.java
+++ recipecalc/src/main/java/recipecalc/calc/Tree.java
@@ -1,23 +1,25 @@
 package recipecalc.calc;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
 import recipecalc.Util;
 import recipecalc.node.LinkedNode;
+import recipecalc.node.ResourceType;
 
 public class Tree {
     public static void printAsTree(LinkedNode origin) {
         Tree tree = new Tree(origin);
         tree.branch();
-        tree.printFirstResource();
+        tree.printPrimaryResource();
         tree.printByProduct();
     }
 
     private Tree(LinkedNode origin) {
         this.origin = origin;
-        firstResource = new HashMap<>();
+        primaryResource = new HashMap<>();
     }
     
     final String LINE = "│";
@@ -27,7 +29,7 @@ public class Tree {
     final String EMPTY = "";
 
     private final LinkedNode origin;
-    private final Map<String, Long> firstResource;
+    private final Map<String, Long> primaryResource;
 
     private void branch() {
         System.out.println("Trees:");
@@ -37,7 +39,7 @@ public class Tree {
     private void branch(LinkedNode node) {
         System.out.println(buildLine(node));
         if (node.child.isEmpty()) {
-            addFirstResource(node);
+            addPrimaryResource(node);
         } else {
             for (LinkedNode cNode : node.child) {
                 branch(cNode);
@@ -65,26 +67,50 @@ public class Tree {
              : EMPTY;
     }
 
-    private void addFirstResource(LinkedNode node) {
-        if (firstResource.containsKey(node.name)) {
-            firstResource.put(node.name, node.craftCount + firstResource.get(node.name));
+    private void addPrimaryResource(LinkedNode node) {
+        if (primaryResource.containsKey(node.name)) {
+            primaryResource.put(node.name, node.craftCount + primaryResource.get(node.name));
         } else {
-            firstResource.put(node.name, node.craftCount);
+            primaryResource.put(node.name, node.craftCount);
         }
     }
 
-    private void printFirstResource() {
+    private void printPrimaryResource() {
         System.out.println("Resource Requirements:");
-        for (Entry<String, Long> entry : firstResource.entrySet()) {
-            System.out.println(" - ".concat(entry.getKey()).concat(":").concat(String.valueOf(entry.getValue())));
-        }
+        prettyPrinter(origin, primaryResource);
     }
 
     private void printByProduct() {
         System.out.println("ByProducts:");
-        for (Entry<String, Long> entry : origin.consumableNode.entrySet()) {
-            if (!entry.getKey().equals(origin.name) && entry.getValue()!=0) {
-                System.out.println(" - ".concat(entry.getKey()).concat(":").concat(String.valueOf(entry.getValue())));
+        prettyPrinter(origin, origin.consumableNode);
+    }
+
+    private static void prettyPrinter(LinkedNode node, Map<String, Long> map) {
+        final long longestKeylength = map.entrySet().stream()
+            .filter(f -> !f.getKey().equals(node.name))
+            .map(m -> m.getKey().length())
+            .max(Comparator.naturalOrder()).get();
+        final long longestValuelength = map.entrySet().stream()
+            .filter(f -> !f.getKey().equals(node.name))
+            .map(m -> String.valueOf(m.getValue()).length())
+            .max(Comparator.naturalOrder()).get();
+        for (Entry<String, Long> entry : map.entrySet()) {
+            if (!entry.getKey().equals(node.name) && entry.getValue()!=0) {
+                System.out.println(
+                    " - "
+                    .concat(entry.getKey())
+                    .concat(Util.repeat(" ", Math.toIntExact(longestKeylength - entry.getKey().length())))
+                    .concat(
+                        node.getNodeByName(entry.getKey()).type.equals(ResourceType.Item)
+                         ? " x"
+                         : ":")
+                    .concat(
+                        node.getNodeByName(entry.getKey()).type.equals(ResourceType.Liquid)
+                         || node.getNodeByName(entry.getKey()).type.equals(ResourceType.Gas)
+                         ? String.valueOf(entry.getValue()).concat("mb")
+                         : Util.repeat(" ", Math.toIntExact(longestValuelength - String.valueOf(entry.getValue()).length()))
+                           .concat(String.valueOf(entry.getValue())))
+                );
             }
         }
     }
