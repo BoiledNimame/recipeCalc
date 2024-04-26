22fc21594970c0ce6d2746b7d5cf6a5280708e79
HadukiLaptop <haduki.kyouka@gmail.com>
2024-04-26 12:44:31 +0900

    副産物と一次リソースのprint作ってほぼ完成だと思う

--- recipecalc/src/main/java/recipecalc/calc/Tree.java
+++ recipecalc/src/main/java/recipecalc/calc/Tree.java
@@ -1,15 +1,22 @@
 package recipecalc.calc;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import recipecalc.Util;
 import recipecalc.node.LinkedNode;
 
 public class Tree {
     public static void printAsTree(LinkedNode origin) {
-        (new Tree()).branch(origin);
+        Tree tree = new Tree();
+        tree.branch(origin);
+        tree.printFirstResource();
+        tree.printByProduct(origin);
         // TODO 一次リソースの集計+print
     }
 
-    private Tree() {}
+    private Tree() { firstResource = new HashMap<>();}
     
     final String LINE = "│";
     final String BRANCH = "├";
@@ -17,10 +24,13 @@ public class Tree {
     final String SPACE = " ";
     final String EMPTY = "";
 
+    private final Map<String, Long> firstResource;
+
     private void branch(LinkedNode node) {
         System.out.println(buildLine(node));
-        if (node.child.isEmpty()) { return; }
-        else {
+        if (node.child.isEmpty()) {
+            addFirstResource(node);
+        } else {
             for (LinkedNode cNode : node.child) {
                 branch(cNode);
             }
@@ -45,5 +55,29 @@ public class Tree {
               ? LINE
               : SPACE)
              : EMPTY;
+    }
+
+    private void addFirstResource(LinkedNode node) {
+        if (firstResource.containsKey(node.name)) {
+            firstResource.put(node.name, node.craftCount + firstResource.get(node.name));
+        } else {
+            firstResource.put(node.name, node.craftCount);
+        }
+    }
+
+    private void printFirstResource() {
+        System.out.println("Resource Requirements:");
+        for (Entry<String, Long> entry : firstResource.entrySet()) {
+            System.out.println(" - ".concat(entry.getKey()).concat(":").concat(String.valueOf(entry.getValue())));
+        }
+    }
+
+    private void printByProduct(LinkedNode origin) {
+        System.out.println("ByProducts:");
+        for (Entry<String, Long> entry : origin.consumableNode.entrySet()) {
+            if (!entry.getKey().equals(origin.name) && entry.getValue()!=0) {
+                System.out.println(" - ".concat(entry.getKey()).concat(":").concat(String.valueOf(entry.getValue())));
+            }
+        }
     }
 }
